e5bafba224a216260c6456813ea553df9d436b80
bartek <bartosz.balinski@wp.pl>
2025-03-26 18:40:16 +0100

    Initial commit

---
+++ src/main/java/com/brighties/backend_brighties/BackEndBrightiesApplication.java
@@ -0,0 +1,13 @@
+package com.brighties.backend_brighties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndBrightiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndBrightiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brighties/backend_brighties/BackEndBrightiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brighties.backend_brighties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndBrightiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
