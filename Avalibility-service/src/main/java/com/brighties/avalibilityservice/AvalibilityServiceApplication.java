bd6f0bb6e628761046523a7f8b621c35b32b385a
bartek <bartosz.balinski@wp.pl>
2025-03-29 16:38:46 +0100

    transitioned to microservice architecture

---
+++ Avalibility-service/src/main/java/com/brighties/avalibilityservice/AvalibilityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brighties.avalibilityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalibilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvalibilityServiceApplication.class, args);
+    }
+
+}
---
+++ Avalibility-service/src/test/java/com/brighties/avalibilityservice/AvalibilityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brighties.avalibilityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalibilityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
