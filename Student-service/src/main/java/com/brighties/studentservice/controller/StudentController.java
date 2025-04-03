ebf94b89f21baa0ffde755351a071c62a3e4ab75
bartek <bartosz.balinski@wp.pl>
2025-04-03 12:40:23 +0200

    added getAllStudents endpoint

---
+++ Student-service/src/main/java/com/brighties/studentservice/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.brighties.studentservice.controller;
+
+
+import com.brighties.studentservice.dto.StudentResponseDTO;
+import com.brighties.studentservice.model.Student;
+import com.brighties.studentservice.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<StudentResponseDTO>> getAllStudents() {
+        List<StudentResponseDTO> students = studentService.getAllStudents();
+        return  ResponseEntity.ok().body(students);
+
+
+    }
+}
