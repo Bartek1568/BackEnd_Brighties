9f33e1ebe95a78f58ea2da7d6a404fdb75620bea
bartek <bartosz.balinski@wp.pl>
2025-04-07 11:50:37 +0200

    added AvailabilitySlot request & response DTO

--- Availability-service/src/main/java/com/brighties/availabilityservice/dto/AvailabilitySlotRequestDTO.java
+++ Availability-service/src/main/java/com/brighties/availabilityservice/dto/AvailabilitySlotRequestDTO.java
@@ -1,4 +1,21 @@
 package com.brighties.availabilityservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class AvailabilitySlotRequestDTO {
+
+    @NotBlank
+    private String teacherId;
+
+    @NotBlank
+    private String date;
+
+    @NotBlank
+    private String dayOfWeek;
+
+    @NotBlank
+    private String startTime;
+
+    @NotBlank
+    private String endTime;
 }
--- Availability-service/src/main/java/com/brighties/availabilityservice/dto/AvailabilitySlotResponseDTO.java
+++ Availability-service/src/main/java/com/brighties/availabilityservice/dto/AvailabilitySlotResponseDTO.java
@@ -1,4 +1,68 @@
 package com.brighties.availabilityservice.dto;
 
+
+
 public class AvailabilitySlotResponseDTO {
+
+    private String id;
+
+    private String teacherId;
+
+    private String date;
+
+    private String dayOfWeek;
+
+    private String startTime;
+
+    private String endTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+
 }
