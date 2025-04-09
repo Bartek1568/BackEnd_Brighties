3145694d0ce51a76c261125f9c264b68f07accd8
bartek <bartosz.balinski@wp.pl>
2025-04-09 14:49:03 +0200

    added reservation response dto

---
+++ Reservation-service/src/main/java/com/brighties/reservationservice/dto/ReservationResponseDTO.java
@@ -0,0 +1,86 @@
+package com.brighties.reservationservice.dto;
+
+public class ReservationResponseDTO {
+
+    private String Id;
+
+    private String teacherId;
+
+    private String studentId;
+
+    private String date;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String status;
+
+    private String note;
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+
+}
