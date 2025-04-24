21ce462ce76b7fbec367a0d9003cd1cf1e5d2a48
bartek <bartosz.balinski@wp.pl>
2025-04-24 13:19:39 +0200

    changed getters and setter to lombok

--- Reservation-service/src/main/java/com/brighties/reservationservice/dto/ReservationRequestDTO.java
+++ Reservation-service/src/main/java/com/brighties/reservationservice/dto/ReservationRequestDTO.java
@@ -1,9 +1,12 @@
 package com.brighties.reservationservice.dto;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -11,24 +14,32 @@ import java.time.LocalTime;
 @Getter
 public class ReservationRequestDTO {
 
+    @NotNull(message = "Teacher ID is required")
     private Long teacherId;
 
+    @NotNull(message = "Student ID is required")
     private Long studentId;
 
+    @NotNull(message = "Availability ID is required")
     private Long availabilityId;
 
+
+    @NotNull(message = "date is required")
+    @FutureOrPresent(message = "Date must be today or in the future")
     private LocalDate date;
 
 
-
-    private DayOfWeek dayOfWeek;
-
+    @NotNull(message = "Start Time is required")
     private LocalTime startTime;
 
+    @NotNull(message = "End Time is required")
     private LocalTime endTime;
 
+    @NotBlank(message = "Status is required")
     private String status;
 
+    @NotBlank(message = "Note is required")
+    @Size(max = 500, message = "Note cannot exceed 500 characters")
     private String note;
 
 }
--- Reservation-service/src/main/java/com/brighties/reservationservice/dto/ReservationResponseDTO.java
+++ Reservation-service/src/main/java/com/brighties/reservationservice/dto/ReservationResponseDTO.java
@@ -1,18 +1,20 @@
 package com.brighties.reservationservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@Setter
+@Getter
 public class ReservationResponseDTO {
 
+    private Long id;
 
     private Long teacherId;
 
     private Long studentId;
-
-
-
-    private Long id;
 
     private Long availabilityId;
 
@@ -25,78 +27,6 @@ public class ReservationResponseDTO {
     private String status;
 
     private String note;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(Long teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public Long getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(Long studentId) {
-        this.studentId = studentId;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public LocalTime getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(LocalTime startTime) {
-        this.startTime = startTime;
-    }
-
-    public LocalTime getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(LocalTime endTime) {
-        this.endTime = endTime;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public Long getAvailabilityId() {
-        return availabilityId;
-    }
-
-    public void setAvailabilityId(Long availabilityId) {
-        this.availabilityId = availabilityId;
-    }
 
 
 }
