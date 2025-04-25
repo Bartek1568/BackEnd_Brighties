093b8c26e4cdd73548e04b1597110c5f388981b0
bartek <bartosz.balinski@wp.pl>
2025-04-25 11:24:32 +0200

    deleted availability slot not found exception

--- Reservation-service/src/main/java/com/brighties/reservationservice/exception/AvailabilitySlotNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.brighties.reservationservice.exception;
-
-public class AvailabilitySlotNotFoundException extends RuntimeException {
-    public AvailabilitySlotNotFoundException(String message) {
-        super(message);
-    }
-}
